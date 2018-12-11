f4ca78debc46ad3b2af9d04479e6afcae2602293
clarkgh <demolisho@hotmail.com>
2018-12-10 17:05:44 -0700

    polymer up aside from transpiling error with ES6 classes, will review to fix tomorrow

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,11 +1,42 @@
-import _ from 'lodash';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+// import _ from 'lodash';
 
-function component() {
-  let element = document.createElement('h1');
+// function component() {
+//   let element = document.createElement('h1');
 
-  element.innerHTML = _.join(['Hello', 'World!'], ' ');
+//   element.innerHTML = _.join(['Hello', 'World!'], ' ');
 
-  return element;
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+class HelloWorld extends PolymerElement {
+  constructor() {    
+    super();
+    this.greeting = 'Hello World!';
+  }
+
+  static get template() {
+    return html`
+      <style>
+        div{
+          background-color: pink;
+        }
+      </style>
+
+      <h1>[[greeting]]</h1>
+    `
+  }
+
+  static get properties() {
+    return {
+      greeting: {
+        type: String
+      }
+    }
+  }
+
 }
 
-document.body.appendChild(component());
+customElements.define('hello-world', HelloWorld)
